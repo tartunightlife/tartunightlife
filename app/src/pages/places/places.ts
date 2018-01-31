6f5542bdee1f975b05efe5c09af6cf0f236dbfe7
abcher <abcher@ttu.ee>
2018-01-31 17:08:14 +0200

    changed about to places

---
+++ app/src/pages/places/places.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-places',
+  templateUrl: 'places.html'
+})
+export class PlacesPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
