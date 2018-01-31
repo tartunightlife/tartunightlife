affa1f88d6a9493fb41a2badb5ba2164015add08
Bruno Produit <bruno.produit@protonmail.ch>
2018-01-31 23:44:43 +0200

    V0.1

--- backend/constants.py
+++ backend/constants.py
@@ -3,7 +3,7 @@
 
 NAME = 'Tartu Night Life Backend' # Name of the app
 AUTHOR = 'Bruno Produit, Basar Turgut, Abel Mesfin, Saumitra Bagchi' # List of authors
-VERSION = '1.1' # Version, constant must be changed at each new milestone
+VERSION = '0.1' # Version, constant must be changed at each new milestone
 SERVER_INET_ADDR = '127.0.0.1' # IP to listen to
 SERVER_PORT = 1234 # Port to listen to
 
