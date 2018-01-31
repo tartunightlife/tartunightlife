89be3f891d4928845d710d29d730d3cb8ce9d6bf
Bruno Produit <bruno.produit@protonmail.ch>
2018-01-31 23:37:55 +0200

    refactor & commenting backend

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
 """
 usage: server.py [-h] [-l LADDR] [-p PORT]
 
@@ -12,58 +13,47 @@ optional arguments:
                         Listen address. Default localhost.
   -p PORT, --port PORT  Listen on port.
 """
-import os
-import threading
+
+#-------------- Imports -------------------------
+
 import logging
 from constants import *
+from facebook_scraper import *
 from argparse import ArgumentParser
 import psycopg2
-import facebook
-import requests
-# Please use LOGGING
-FORMAT = '%(asctime)-15s %(levelname)s %(threadName)s %(message)s'
-logging.basicConfig(level=logging.DEBUG, format=FORMAT)
-LOG = logging.getLogger()
+
+#-------------- Methods -------------------------
 
 def initialize():
-    # Connect to DB
-    database = open('.pgpass', 'r').read().split(':')
-    #conn = psycopg2.connect(host=database[0], port=int(database[1]), database=database[2], user=database[3], password=database[4])
-    #pg = conn.cursor()
+    # Logging
+    FORMAT = '%(asctime)-15s %(levelname)s %(threadName)s %(message)s'
+    logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+    LOG = logging.getLogger()
 
-    # Init Facebook Graph API
-    client = open('.facebook', 'r').read().strip().split(':')
-    payload = {'client_id': client[0], 'client_secret': client[1], 'grant_type' : 'client_credentials'}
-    token = requests.get("https://graph.facebook.com/oauth/access_token", params=payload)
-    token = token.json()['access_token']
-    fb = facebook.GraphAPI(token)
-
-def info(): return '%s by %s, version %s' % (NAME, AUTHOR, VERSION)
-
-class FacebookScraper:
-    def __init__(self):
-        LOG.info("Facebook thread started.")
-
-    def get_events(self):
-        events = []
-        for i in PLACES:
-            events.append(fb.get_object(id=i, fields='events'))
-        return events
-
-        #self.server_thread = threading.Thread(target=self.server.serve_forever)
-        #self.server_thread.start()
-        #self.server_thread.join()
-
-if __name__=="__main__":
+    # Arguments
     parser = ArgumentParser(description=info())
     parser.add_argument('-l', '--laddr', help="Listen address. Default localhost.",  default=SERVER_INET_ADDR)
     parser.add_argument('-p', '--port', help="Listen on port.", default=SERVER_PORT, type=int)
     args = parser.parse_args()
 
+    # Database
+    database = open('.pgpass', 'r').read().split(':')
+    #conn = psycopg2.connect(host=database[0], port=int(database[1]), database=database[2], user=database[3], password=database[4])
+    #pg = conn.cursor()
+
+def info(): return '%s by %s, version %s' % (NAME, AUTHOR, VERSION)
+
+
+#-------------- Main -------------------------
+
+if __name__=="__main__":
     try:
+        # Init
         initialize()
-        face = FacebookScraper()
-        events = face.get_events()
+
+        # Facebook Scraping
+        fb = FacebookScraper()
+        events = fb.get_events()
         print (events)
 
 
