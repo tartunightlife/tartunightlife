fd264e3e1f5297ace36b52b23904d506daf214a0
abcher <abel2mesfin@gmail.com>
2018-02-01 15:28:32 +0200

    showed list of events in the place page

--- app/src/pages/place/place.ts
+++ app/src/pages/place/place.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { EventPage } from '../event/event';
 
 @Component({
   selector: 'page-place',
@@ -7,9 +8,12 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class PlacePage {
   place;
-  constructor(params: NavParams) {
+  constructor(params: NavParams, public navCtrl: NavController) {
     this.place = params.data.place;
   }
 
+  openEventPage(event) {
+    this.navCtrl.push(EventPage, { event: event});
+  }
 }
 
--- app/src/pages/places/places.ts
+++ app/src/pages/places/places.ts
@@ -10,9 +10,12 @@ export class PlacesPage {
   places = [];
   constructor(public navCtrl: NavController) {
     this.places = [
-                   {id:1, name:'moku', address:'moku-address', url:'moku-url'},
-                   {id:2, name:'Sooters', address:'Sooters-address', url:'Sooters-url'},
-                   {id:3, name:'Illusion', address:'Illusion-address', url:'Illusion-url'}
+                   {id:1, events: [{id:1, place_id:1, place_name:"moku", name:'moku-event', date:'moku-event-date', 
+                    url:'moku-event-url'}], name:'moku', address:'moku-address', url:'moku-url'},
+                   {id:2, events:[{id:2, place_id:1, place_name:"Sooters", name:'Sooters-event', date:'Sooters-event-date', 
+                    url:'Sooters-event-url'}], name:'Sooters', address:'Sooters-address', url:'Sooters-url'},
+                   {id:3, events:[{id:3, place_id:1, place_name:"Illusion", name:'Illusion-event', date:'Illusion-event-date', 
+                    url:'Illusion-event-url'}],name:'Illusion', address:'Illusion-address', url:'Illusion-url'}
                   ];
   }
 
